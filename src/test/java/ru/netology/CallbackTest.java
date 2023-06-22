6c1c05d7e44979a8f7ffd660f3bca10eac7b78ba
Pavel Shvydkin <Pshvydkin@gmail.com>
2023-06-22 16:28:36 +0300

    update

---
+++ src/test/java/ru/netology/CallbackTest.java
@@ -0,0 +1,39 @@
+package ru.netology;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class CallbackTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldCallbackTest() throws InterruptedException {
+        // загрузка страницы
+       open("http://0.0.0.0:9999");
+    }
+
+}
