e618a18622581ad3a535ce04b286db90aa424366
Pavel Shvydkin <Pshvydkin@gmail.com>
2023-06-22 19:45:56 +0300

    update test

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -5,10 +5,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallbackTest {
 
@@ -33,7 +36,13 @@ public class CallbackTest {
     @Test
     void shouldCallbackTest()  {
         // загрузка страницы
-       open("http://0.0.0.0:9999");
+        driver.get("http://localhost:9999");
+        driver.findElement((By.cssSelector("[data-test-id=name] input"))).sendKeys("Вася Пупкин");
+        driver.findElement((By.cssSelector("[data-test-id=phone] input"))).sendKeys("+79991234567");
+        driver.findElement((By.cssSelector("[data-test-id=agreement]"))).click();
+        driver.findElement(By.cssSelector("button.button.button_view_extra.button_size_m.button_theme_alfa-on-white")).click();
+        String text = driver.findElement((By.cssSelector("[data-test-id=order-success]"))).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
 }
