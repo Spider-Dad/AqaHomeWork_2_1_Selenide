b22dd740d7999860d1d41c4542ad10db4d2fc68c
Pavel Shvydkin <Pshvydkin@gmail.com>
2023-06-25 21:41:13 +0300

    Update CallbackTest modified notPushAgreementTest

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -129,11 +129,16 @@ public class CallbackTest {
         driver.findElement((By.cssSelector("[data-test-id=name] input"))).sendKeys("Николай Римский-Корсаков");
         driver.findElement((By.cssSelector("[data-test-id=phone] input"))).sendKeys("+799912345677");
         WebElement agreementCheckbox = driver.findElement((By.cssSelector("[data-test-id=agreement]")));
+
+        boolean isCheckboxDisplayed = agreementCheckbox.isDisplayed();
+        Assertions.assertTrue(isCheckboxDisplayed);
+
         boolean isChecked = agreementCheckbox.isSelected();
         Assertions.assertFalse(isChecked);
+
         driver.findElement(By.cssSelector("button")).click();
-        Assertions.assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", driver.findElement(By.cssSelector("[data-test-id=agreement].checkbox .checkbox__text")).getText().trim());
-        Assertions.assertEquals(driver.findElement(By.cssSelector(".input_invalid")).getCssValue("color"), "rgba(255, 92, 92, 1)");
+
+
     }
 
 }
